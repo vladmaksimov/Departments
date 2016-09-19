de10fe431e8b2ae751016d58db76db93058791b4
Vladislav Maksimov <v.maksimov@aimprosoft.com>
2016-09-19 18:52:01 +0300

    Added Java-Doc to page service interface

--- src/main/src/com/maksimov/services/PageService.java
+++ src/main/src/com/maksimov/services/PageService.java
@@ -4,8 +4,9 @@ import com.maksimov.data.Pageable;
 import com.maksimov.exceptions.DepartmentException;
 
 /**
- *  * Abstract interface for pagination information.
+ * Abstract interface for pagination information.
  *
+ * @author Vlad Maksimov
  * Created on 16.09.16.
  */
 public interface PageService {
@@ -13,14 +14,25 @@ public interface PageService {
     Integer PAGE_DEFAULT_TOTAL = 1;
 
     /**
-     * Gets the additional data of the {@link Pageable} object, like total page count
+     * Gets the additional data of the {@link Pageable} object, like total page count of departments having next or
+     * previous page, etc.
      *
      * @param pageable instance of the {@link Pageable} object.
-     * @param search {@link String} value needed to correct information for page object
+     * @param search   {@link String} value needed to correct information for page object
      * @return the {@link Pageable} object with full page information
      * @throws DepartmentException related with database issue.
      */
     Pageable getDepartmentPageDetails(Pageable pageable, String search) throws DepartmentException;
 
+    /**
+     * Gets the additional data of the {@link Pageable} object, like total page count of employees, having next or
+     * previous page, etc.
+     *
+     * @param pageable instance of the {@link Pageable} object.
+     * @param id       {@link Long} value of the {@link com.maksimov.models.Department} object.
+     * @param search   {@link String} value needed to correct information for page object
+     * @return the {@link Pageable} object with full page information
+     * @throws DepartmentException related with database issue.
+     */
     Pageable getEmployeePageDetails(Pageable pageable, Long id, String search) throws DepartmentException;
 }
